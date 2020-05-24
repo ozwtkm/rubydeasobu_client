b2abde78bd59b92491313dc49a7f81fc12d8eb42
ozawa <ozwtkm3062@gmail.com>
2020-05-24 19:04:58 +0900

    clean: Scriptフォルダ構成整理した

---
+++ Assets/Script/Common/LoginManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO; // ←追加
+using System.Runtime.Serialization.Json; // ←追加
+using GeneralPurpose;
+using System.Text;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using System.Text.RegularExpressions;
+
+ 
+public class LoginManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+ 
+    public void Login()
+    {
+        //ログインユーザ名を取得
+        Text usernameobj = GameObject.Find("Input_name/Text").GetComponent<Text>();
+        Text passwordobj = GameObject.Find("Input_password/Text").GetComponent<Text>();
+
+        //Text型をstring型に変換
+        string username = usernameobj.text;
+        string password = passwordobj.text;
+
+        //To Do:シリアライズちゃんとする
+        string json = "[\"" + username + "\", \"" + password + "\"]";
+
+        //string json = "[\"kkk\",\"k\"]";
+        
+        string URL = "http://rqmul.wfm.jp/login";
+
+        StartCoroutine(Post(URL, json));
+    }
+ 
+
+    IEnumerator Post(string url, string bodyJsonString){
+        UnityWebRequest request = new UnityWebRequest(url, "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(bodyJsonString);
+        request.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.Send();
+
+        string setcookieheader = request.GetResponseHeader("Set-cookie");
+    
+        // To Do: 正規表現ちゃんと考える
+        Match matche = Regex.Match(setcookieheader, "session_id=(.*)");
+        
+        string cookie = matche.Groups[1].Value;
+
+        // to do: cookieの管理方法ちゃんと調べる
+        PlayerPrefs.SetString("session_id", cookie);
+
+        DataContractJsonSerializerSettings settings = new DataContractJsonSerializerSettings();
+        settings.MaxItemsInObjectGraph = 10; 
+
+        string json = request.downloadHandler.text;
+        Text Messageobj = GameObject.Find("Message").GetComponent<Text>();
+
+        if (request.responseCode == 200){
+            Username data = JsonUtils.ToObject<Username>(json);
+
+            Messageobj.text = "ようこそ " + data.username + " !!";
+
+            PlayerPrefs.SetString("username", data.username);
+
+            print("LOGIN");
+        }else{ // todo 配列で返ってきたパターンでちゃんと表示できるようにする
+            ErrorResponse errorobj = JsonUtils.ToObject<ErrorResponse>(json);
+            //errorobj.message = "test";
+            Messageobj.text = errorobj.ErrorMessage;
+
+            print("LOGINFAILED");
+        }
+    }
+ 
+}
---
+++ Assets/Script/Common/WebsocketClient.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine.SceneManagement;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+using System.Text;
+
+using GeneralPurpose;
+
+
+
+
+public class WebsocketClient: MonoBehaviour {
+
+    WebSocket ws;
+    private const string URL = "ws://rqmul.wfm.jp/unko";
+    private List<string> messageque = new List<string>();
+
+    void Start()
+    {
+
+        ws = new WebSocket(URL);
+
+        string cookiename = "session_id";
+        string cookievalue = PlayerPrefs.GetString("session_id");
+        ws.SetCookie (new Cookie (cookiename, cookievalue));
+
+        ws.OnOpen += (sender, e) =>
+        {
+            Debug.Log("WebSocket Open");
+        };
+
+        ws.OnMessage += (sender, e) =>
+        {
+            //ここで直接レンダリングしようとするとmainではないスレッドになるのでGamObject探索とかするときエラーになる
+            messageque.Add(e.Data);
+            
+            Debug.Log("WebSocket Message Type: " + e.GetType() + ", Data: " + e.Data);
+        };
+
+        ws.OnError += (sender, e) =>
+        {Dbg.s(e);
+            Debug.Log("WebSocket Error Message: " + e.Message);
+        };
+
+        ws.OnClose += (sender, e) =>
+        {
+            Debug.Log("WebSocket Close");
+        };
+
+        ws.Connect();
+
+    }
+
+    void Update()
+    {
+        foreach(string message in messageque){
+            WebsocketClientUtil.DisplayMessage(message);
+        }
+
+        messageque.Clear();
+
+    }
+
+    private void DisplayMessage(MessageEventArgs e){
+            GameObject messageobj = GameObject.Find("Messageobj");
+            //GameObject clone = GameObjectUtils.Clone(messageobj);
+            //clone.GetComponent<Text>().text = "gergeargehea";
+    }
+
+
+    public void SendMessage(){
+        Text remarkobj = GameObject.Find("ChatForm/Text").GetComponent<Text>();
+        string remark = remarkobj.text;
+
+        ws.Send(remark);
+    }
+
+    public void Exit(){
+        ws.Close();
+        ws = null;
+    }
+
+    public void SendTest()
+    {
+        ws.Send("Test Message From Client");
+        Debug.Log("Done SendTest");
+    }
+
+    void OnDestroy()
+    {
+        ws.Close();
+        ws = null;
+    }
+}
+
+
+public class WebsocketClientUtil{
+    public static void DisplayMessage(string message){
+            GameObject messageobj = GameObject.Find("Messageobj");
+            GameObject clone = GameObjectUtils.Clone(messageobj);
+            clone.GetComponent<Text>().text = message;
+    }}
